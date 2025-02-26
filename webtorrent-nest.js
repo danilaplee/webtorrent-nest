4a0ea18008e7df41155913821fea9b248b8447bf
Daniel <fiend.of.sound@gmail.com>
2025-02-26 22:49:48 +0100

    feat: add workflo

--- webtorrent-nest.js
+++ webtorrent-nest.js
@@ -1,11 +1,27 @@
 const http = require('http');
 const { spawn } = require('node:child_process');
 
-const children = []
+const children = {}
 const streamFile = (req, res) => {
   const magnetUri = decodeURIComponent(req.url.split('magnet=')?.[1]?.split('&')?.[0])
-  const cmd = `webtorrent download "${magnetUri}" --keep-seeding`
-  children.push(spawn(cmd, {cwd:"/webtorrent"}))
+  if(children[magnetUri]) {
+    res.write(JSON.stringify({"res":"done"}))
+    res.end()
+    return;
+  }
+  const child = spawn("webtorrent", ["download", magnetUri, "--keep-seeding"], 
+  //   (error, stdout, stderr) => {
+  //   if (error) {
+  //     throw error;
+  //   }
+  //   console.log(stdout);
+  // }
+    // {cwd:"/webtorrent"}
+  )
+  child.stdout.on('data', (data) => {
+    console.info('logs', data.toString())
+  });
+  children[magnetUri] = child
   res.write(JSON.stringify({"res":"done"}))
   res.end()
 }
@@ -16,6 +32,7 @@ http.createServer((req, res)=> {
     try {
       streamFile(req, res)
     } catch(err) {
+      console.error('errror', err)
       res.write(JSON.stringify({"res":"error"}))
       res.end()
     }
