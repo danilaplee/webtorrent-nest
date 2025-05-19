8d7d867e9ed8461539cec2d5df728c9805a273cc
danilaplee <fiend.of.sound@gmail.com>
2025-05-19 02:32:52 +0200

    feat: psql

--- webtorrent-thread.js
+++ webtorrent-thread.js
@@ -12,7 +12,7 @@ const runSeed = async () => {
   let cache;
   try {
     cache = await Files.findByPk(id)
-    torrentFile = Uint8Array.from(Object.values(cache.torrentFile.data))
+    torrentFile = Uint8Array.from(Object.values(cache.torrentFile))
   } catch (err) {
     console.error('torrentFile parsing error', err)
     // throw err;
